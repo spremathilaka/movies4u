81859a957200b6689060980efb65282438dc5d0e
samith premathilaka <zotikossoft@gmail.com>
2019-03-27 16:46:22 +1100

    add api service

--- app/src/main/java/com/zotikos/m4u/data/remote/ApiService.kt
+++ app/src/main/java/com/zotikos/m4u/data/remote/ApiService.kt
@@ -1,12 +1,12 @@
 package com.zotikos.m4u.data.remote
 
-import com.zotikos.m4u.model.Post
+import com.zotikos.m4u.data.model.Post
 import io.reactivex.Observable
+
 import retrofit2.http.GET
 
-interface PostApi {
+interface ApiService {
 
     @GET("/posts")
     fun getPosts(): Observable<List<Post>>
-
 }
