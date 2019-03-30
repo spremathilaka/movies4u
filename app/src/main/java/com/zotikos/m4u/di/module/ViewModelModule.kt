071eb437f5974c74aa9dfcb0982f2df4eff07d1b
samith premathilaka <zotikossoft@gmail.com>
2019-03-30 18:53:00 +1100

    adding view model (AAC)

--- app/src/main/java/com/zotikos/m4u/di/module/ViewModelModule.kt
+++ app/src/main/java/com/zotikos/m4u/di/module/ViewModelModule.kt
@@ -1,4 +1,50 @@
 package com.zotikos.m4u.di.module
 
+import androidx.lifecycle.ViewModel
+import com.zotikos.m4u.data.repository.PostRepository
+import com.zotikos.m4u.ui.detail.PostDetailsViewModel
+import com.zotikos.m4u.ui.main.MainActivityViewModel
+import com.zotikos.m4u.ui.posts.PostListViewModel
+import com.zotikos.m4u.util.SchedulerProvider
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoMap
+import javax.inject.Provider
+
+@Module
 class ViewModelModule {
+
+
+    //error: A @Module may not contain both non-static @Provides methods and abstract @Binds or @Multibinds declarations
+    // @Binds
+    // abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+
+    @Provides
+    fun providesViewModelFactory(viewModelMap: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>): ViewModelFactory {
+        return ViewModelFactory(viewModelMap)
+    }
+
+    @Provides
+    @IntoMap
+    @ViewModelKey(PostListViewModel::class)
+    fun providePostListViewModel(repository: PostRepository, schedulerProvider: SchedulerProvider): ViewModel {
+        return PostListViewModel(repository, schedulerProvider)
+    }
+
+    @Provides
+    @IntoMap
+    @ViewModelKey(PostDetailsViewModel::class)
+    fun providePostDetailsViewModel(repository: PostRepository, schedulerProvider: SchedulerProvider): ViewModel {
+        return PostDetailsViewModel(repository, schedulerProvider)
+    }
+
+
+    @Provides
+    @IntoMap
+    @ViewModelKey(MainActivityViewModel::class)
+    fun bindMainViewModel(): ViewModel {
+        return MainActivityViewModel()
+    }
+    //Add more ViewModels here
 }
